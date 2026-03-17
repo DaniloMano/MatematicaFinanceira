4c0f0428e39e3c862134466fa277f865f653e4da
danilo <danilogabrill@outlook.com>
2026-03-17 17:17:11 -0300

    first commit

---
+++ PAfunc.py
@@ -0,0 +1,11 @@
+# Atividade nº 1 do Professor Márcio:
+# fazer função simples de PA em python usando linspace como no exemplo do professor:
+# Implementação de exemplo:
+# np.linspace(a,b,n)
+# vetor
+# a1=1
+# a100=10
+# n=100
+# r=?
+# an = a1+ (n-1)*r
+# r=(an-a1)/(n-1)
